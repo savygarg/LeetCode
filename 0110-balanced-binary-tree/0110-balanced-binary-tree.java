1b7ec3b49c6d7b34e538db1c90f39c6fe0695c95
Savy Garg <savygarg2006@gmail.com>
2026-09-04 15:48:55 +0530

    Time: 0 ms (100%), Space: 45.2 MB (95.08%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -30,8 +30,8 @@ class Solution {
             return -1;
         }
         if(Math.abs(left - right) > 1){
-            return - 1;
+            return -1;
         }
-        return Math.max(left,right)+1;
+        return Math.max(left,right) + 1;
     }
 }
